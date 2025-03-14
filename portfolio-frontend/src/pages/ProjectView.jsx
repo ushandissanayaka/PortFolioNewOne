2c68bb9578baf9b641ab825e42547c15b5784add
ushan pramod <125153601+ushandissanayaka@users.noreply.github.com>
2025-03-15 00:16:38 +0530

    portfolio frontend

---
+++ portfolio-frontend/src/pages/ProjectView.jsx
@@ -0,0 +1,9 @@
+// src/pages/ProjectView.jsx
+export default function ProjectView() {
+    return (
+      <div>
+        <h1>Project Details</h1>
+        <p>This is the project view page.</p>
+      </div>
+    );
+  }
